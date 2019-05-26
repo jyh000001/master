2c750d0ffe6df5d509d6328420387c0229ef3e48
jyh000001 <jyh000001@126.com>
2019-05-26 11:46:37 +0800

    add test pro

---
+++ test/src/test/java/com/springboot/test/test/FreeTest.java
@@ -0,0 +1,16 @@
+package com.springboot.test.test;
+
+import java.util.Date;
+
+import ch.qos.logback.core.util.TimeUtil;
+
+public class FreeTest {
+
+	public static void main(String[] args) {
+		
+		Date date = new Date(TimeUtil.);
+		System.out.println(date);
+
+	}
+
+}
